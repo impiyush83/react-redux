cb7490ba35c96f9bb0ccb792cd20d77cc75a4d3a
impiyush83 <nalawadepiyush@gmail.com>
2020-01-13 00:16:54 +0530

    module: completed module 2 from https://www.udemy.com/course/react-redux/

--- src/index.js
+++ src/index.js
@@ -2,8 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText() {
+    return 'Click Me!'
+}
+
+function getTime() {
+    return (new Date()).toLocaleTimeString()
+}
+
 const App = () => {
-    return <div>Hello !</div>
+    return (
+        <div>
+                <div>Current Time:</div>
+                <h3>{getTime()}</h3>
+            </div>
+        // <div>
+        // <label className="label" HTMLfor="name">Enter Name:</label> 
+        // <input id="name" type="text" />
+        // <button style={{ backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+        // </div>
+    )
 };
 
 ReactDOM.render(
